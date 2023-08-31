64fbbd708f11f39d2dbed93ca30be5f2b7b46a95
jan2816 <jan99lauch@gmail.com>
2023-08-31 07:14:00 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace PictureBoxMovementExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PictureBoxMovementExample.Properties.Resources.aesthetic_nature;
+            this.pictureBox1.Location = new System.Drawing.Point(344, 388);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::PictureBoxMovementExample.Properties.Resources.aesthetic_nature;
+            this.pictureBox2.Location = new System.Drawing.Point(344, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::PictureBoxMovementExample.Properties.Resources.aesthetic_nature;
+            this.pictureBox3.Location = new System.Drawing.Point(387, 47);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(10, 15);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureBoxMovementExample
+{
+    public partial class Form1 : Form
+    {
+
+        private const int pictureBox1Speed = 5;
+        private const int pictureBox2Speed = 10;
+        private const int pictureBox2YOffset = 50;
+        private bool pictureBox2MovingLeft = true;
+        private int pictureBox2MoveCount = 0;
+        private List<PictureBox> pictureBoxesToAvoid = new List<PictureBox>(); // Lista de pictureBoxes lanzados
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+            // Configurar eventos de teclado
+            KeyDown += Form1_KeyDown;
+
+            // Configurar temporizador para mover pictureBox2
+            Timer timer = new Timer();
+            timer.Interval = 50;
+            timer.Tick += timer1_Tick;
+            timer.Start();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Mover pictureBox1 de izquierda a derecha (limitando dentro del formulario)
+            if (e.KeyCode == Keys.Left)
+            {
+                if (pictureBox1.Left - pictureBox1Speed >= 0)
+                {
+                    pictureBox1.Left -= pictureBox1Speed;
+                }
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                if (pictureBox1.Right + pictureBox1Speed <= ClientSize.Width)
+                {
+                    pictureBox1.Left += pictureBox1Speed;
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+                // Mover pictureBox2 de derecha a izquierda y hacia abajo (limitando dentro del formulario)
+
+
+
+                if (pictureBox2MovingLeft)
+            { 
+                if (pictureBox2.Left - pictureBox2Speed >= 0)
+                {
+                    pictureBox2.Left -= pictureBox2Speed;
+                }
+                else
+                {
+                    pictureBox2MovingLeft = false;
+                    pictureBox2MoveCount++;
+                }
+            }
+            else
+            {
+                if (pictureBox2.Right + pictureBox2Speed <= ClientSize.Width)
+                {
+                    pictureBox2.Left += pictureBox2Speed;
+                }
+                else
+                {
+                    pictureBox2MovingLeft = true;
+                    if (pictureBox2MoveCount >= 2)
+
+                    {
+                          pictureBox2MoveCount = 0;
+                           if (pictureBox2.Top + pictureBox2YOffset <=                    ClientSize.Height - pictureBox2.Height)
+                            {
+                            pictureBox2.Top += pictureBox2YOffset;
+                           }
+                        else
+                        {
+                                 if (pictureBox2.Bounds.IntersectsWith(pictureBox1.Bounds))
+                                 {
+                                Application.Exit();
+                                 }
+                                    else
+                                    {
+                                     // Cerrar la aplicación cuando llega final
+                                        Application.Exit();
+                                    }
+                        }
+
+
+
+
+                    }
+                }
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            // Lanzar un nuevo PictureBox de manera aleatoria con una probabilidad del 10% en cada intervalo
+            if (pictureBox2MoveCount % 3 == 0 && pictureBox2MoveCount > 0 && new Random().Next(0, 10) == 0)
+            {
+                PictureBox newPictureBox = new PictureBox();
+                newPictureBox.BackColor = Color.Green;
+                newPictureBox.Size = new Size(30, 30);
+                newPictureBox.Location = new Point(pictureBox2.Left, pictureBox2.Top);
+                Controls.Add(newPictureBox);
+                pictureBoxesToAvoid.Add(newPictureBox);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
